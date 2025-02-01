df9f14f2bb5ade72fe1418f0da396e012c1fb903
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-02-01 22:02:51 +0700

    task 7, 5

--- for.js
+++ for.js
@@ -1,4 +1,8 @@
-let color = ["red", "blue", "green", "yellow", "orange", "purple"];
-for (let i = 0; i < color.length; i++) {
-  console.log(color[i]);
+let names = ["Nicholas", "Marshall", "Ryan", "Katie", "Amelia"];
+for (let i = 0; i < names.length; i++) {
+  if (names[i].length > 6) {
+    console.log("สวัสดีคุณ " + names[i]);
+  } else {
+    console.log("Hello " + names[i]);
+  }
 }
