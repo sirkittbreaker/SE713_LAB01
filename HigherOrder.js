f6a4ba164a45ae99302edc9eb5e22f6093df8ac4
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-02-01 23:00:55 +0700

    task 12, 3

--- HigherOrder.js
+++ HigherOrder.js
@@ -1,4 +1,5 @@
-const n = [1, 2, 3, 4, 5];
-const even = n.filter((num) => num % 2 === 0);
+const n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const div3 = n.filter((num) => num % 3 === 0);
+const power3 = div3.map((num) => num ** 3);
 
-console.log(even);
+console.log(power3);
