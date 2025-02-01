9553e86d1698477b42f07dbe14a47cd0f52c8896
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-02-01 22:05:32 +0700

    task 8, 1

--- for.js
+++ for.js
@@ -1,8 +1,5 @@
-let names = ["Nicholas", "Marshall", "Ryan", "Katie", "Amelia"];
-for (let i = 0; i < names.length; i++) {
-  if (names[i].length > 6) {
-    console.log("สวัสดีคุณ " + names[i]);
-  } else {
-    console.log("Hello " + names[i]);
-  }
+function greet(name) {
+  console.log("Hello " + name);
 }
+
+greet("John");
